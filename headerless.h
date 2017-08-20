0d5feee12b3ebd957a6d7b5a1340d806f105e914
Ryan Hilbert <Ryan.J.Hilbert@gmail.com>
2017-08-19 23:17:48 -0400

    Create headerless.h

---
+++ headerless.h
@@ -0,0 +1,37 @@
+#ifdef F
+#ifdef H
+#include F
+#else
+#define H
+#include F
+#undef H
+#endif
+#endif
+
+#undef B
+#ifdef H
+#define B(...);
+#else
+#define B(...){__VA_ARGS__}
+#endif
+
+#undef C
+#ifdef __cplusplus
+#define C extern"C"
+#else
+#define C
+#endif
+
+#undef E
+#ifdef H
+#define E(...);
+#else
+#define E(...)=__VA_ARGS__;
+#endif
+
+#undef S
+#ifdef H
+#define S(...)
+#else
+#define S(...)static __VA_ARGS__
+#endif
